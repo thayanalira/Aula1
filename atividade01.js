4d8e923be64c826c147d7facdda3d4b4ee97a76d
Thayana Lira <santos.thayana21@gmail.com>
2024-09-30 20:24:28 -0300

    atualizando github

---
+++ atividade01.js
@@ -0,0 +1,15 @@
+var nome = "Thayana"
+var idade = 33
+console.log("Olá, meu nome é" + nome + "e tenho" + idade + "anos")
+
+var nome2 = "Jõao"
+var cidade = "Olinda"
+console.log(nome2 + " é de " + cidade)
+
+var num1 = 10
+var num2 = 5
+var soma = num1 + num2
+var subtracao = num1 - num2
+var multiplicacao = num1 * num2
+var divisao = num1 / num2
+console.log(" soma= " + soma + ", subtração=" + subtracao + ", multiplicação=" + multiplicacao + ", " + divisao)
